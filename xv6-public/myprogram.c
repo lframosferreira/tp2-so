34163a20ec0c7e3c3a0712b51a001d83a464b5f3
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-11-14 21:43:05 -0300

    algumas correções no report e adicionando xv6-public ao repositório

---
+++ xv6-public/myprogram.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "user.h"
+#include "stat.h"
+int main(void)
+{  
+    printf(1, "Hello World! %d\n", change_prio());  
+    exit();
+}
---
+++ xv6-public/param.h
@@ -0,0 +1,17 @@
+#define NPROC        64  // maximum number of processes
+#define KSTACKSIZE 4096  // size of per-process kernel stack
+#define NCPU          8  // maximum number of CPUs
+#define NOFILE       16  // open files per process
+#define NFILE       100  // open files per system
+#define NINODE       50  // maximum number of active i-nodes
+#define NDEV         10  // maximum major device number
+#define ROOTDEV       1  // device number of file system root disk
+#define MAXARG       32  // max exec arguments
+#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
+#define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
+#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+#define FSSIZE       1000  // size of file system in blocks
+
+#define INTERV 5 // interval between preemptions
+#define T_1TO2 200 //Promove um processo da fila 1 para fila 2 se tempo de espera maior que 1TO2 ticks
+#define T_2TO3 100 //Promove um processo da fila 2 para fila 3 se tempo de espera maior que 2TO3 ticks
